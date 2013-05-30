2027901068bc582210aa3279087ea5f061592e98
Jeremy Lubin <lubin.jeremy@gmail.com>
2013-05-29 23:57:37 -0700

    Reformat add meeting

---
+++ Queue/LocationChooserViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationChooserViewController.h
+//  Queue
+//
+//  Created by Jeremy Lubin on 5/29/13.
+//  Copyright (c) 2013 Lubin Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationChooserViewController : UIViewController
+
+@end
